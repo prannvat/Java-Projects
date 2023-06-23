a1913340600679dd3eadcff0088e8d1fe10e8160
prannvat <53794605+prannvat@users.noreply.github.com>
2023-06-23 11:59:40 +0100

    Sto Still very basic

---
+++ primitive-types-and-strings/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + " and is a data type";
+        System.out.println(myString);
+
+        int Result = 1 + 2;
+        System.out.println(Result);
+        int previousResult = Result * 2;
+        int remainder = previousResult % 2;
+        System.out.println(remainder + " is the remainder");
+        boolean isAlien = false;
+        if(isAlien == false)
+            System.out.println("it is not an alien");
+
+
+
+    }
+}
